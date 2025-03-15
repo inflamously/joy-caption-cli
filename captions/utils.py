b6a602af13de6eb2968aab05a0e855168855e0ad
nflamously <david.dembinski.1996@gmail.com>
2025-03-15 20:30:45 +0100

    feat: remove stupid ui

--- captions/utils.py
+++
@@ -1,7 +0,0 @@
-
-# File: utils.py
-# Author: nflamously
-# Original License: Apache License 2.0
-
-def break_list_into_chunks(inputs: list, chunk_size: int) -> list:
-    return [inputs[i * chunk_size:(i + 1) * chunk_size] for i in range((len(inputs) + chunk_size - 1) // chunk_size)]
--- config/load.py
+++
@@ -1,7 +0,0 @@
-import json
-from typing import Dict, Any
-
-
-def load_config(path: str) -> Dict[str, Any]:
-    with open(path, 'r') as jsonfile:
-        return json.load(jsonfile)
